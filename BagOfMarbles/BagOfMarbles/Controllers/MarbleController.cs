cab711d3c39e9b0ad269282bbc5314f183bd3e19
blaise594 <danielbrogers594@gmail.com>
2017-04-26 15:20:57 -0400

    first commit

---
+++ BagOfMarbles/BagOfMarbles/Controllers/MarbleController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BagOfMarbles.Models;
+
+namespace BagOfMarbles.Controllers
+{
+    public class MarbleController : ApiController
+    {
+        public static List<Marble> listofmarbles = new List<Models.Marble>
+        {
+            new Marble{Id = 1, Color = "Blue"},
+
+            new Marble{Id = 2, Color = "Red"},
+
+            new Marble{Id = 3, Color = "Yellow"},
+        };
+
+        public IHttpActionResult Get()
+        {
+            return Ok(listofmarbles);
+        }
+
+        public IHttpActionResult GetRandomMarble(int count)
+        {
+
+        }
+    }
+}
---
+++ BagOfMarbles/BagOfMarbles/Scripts/main.js
@@ -0,0 +1,19 @@
+﻿console.log("loaded main.js");
+
+let LoadTheMarbles = () => {
+
+    $.ajax({
+        url: "/api/marble",
+        datatype: "json",
+        success: (marble) => {
+           marble.map((n) => {
+            $("#BagOfMarbles")
+                .append($("<li>").html(m.Color))
+
+           });
+        }
+    });
+
+$(document).ready(() => {
+    console.log("ready...");
+});
